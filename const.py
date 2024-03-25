b7d2fa9ad91d5cb845b6eaad8b35df98773f0630
Mateusz <matgalv2@gmail.com>
2024-03-25 10:30:59 +0100

    Updated columns

--- const.py
+++ const.py
@@ -1,15 +1,14 @@
-seasons = [
-    "22018",
-    "22019",
-    "22020",
-    "22021",
-    "22022",
-    "22023",
+game_log_columns = [
+    'Team_ID', 'Game_ID', 'GAME_DATE', 'MATCHUP',
+    'WL', 'W', 'L', 'W_PCT', 'MIN', 'FGM', 'FGA',
+    'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM',
+    'FTA', 'FT_PCT', 'OREB', 'DREB', 'REB', 'AST',
+    'STL', 'BLK', 'TOV', 'PF', 'PTS'
 ]
 
-game_log_columns = ['Team_ID', 'Game_ID', 'GAME_DATE', 'MATCHUP', 'WL', 'W', 'L', 'W_PCT',
-                    'MIN', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA',
-                    'FT_PCT', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF',
-                    'PTS']
-
-advanced_game_details_columns = ['Game_ID', 'Team_ID', 'OFF_RATING', 'DEF_RATING']
+advanced_game_details_columns = [
+    'Game_ID', 'Team_ID', 'OFF_RATING', 'DEF_RATING',
+    'NET_RATING', 'AST_PCT', 'AST_TOV', 'AST_RATIO',
+    'OREB_PCT', 'DREB_PCT', 'REB_PCT', 'TM_TOV_PCT',
+    'EFG_PCT', 'TS_PCT', 'PACE', 'PIE'
+]
