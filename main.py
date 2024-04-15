11be9faea6e74850eda213abb8d08ac604982ed5
Mateusz <matgalv2@gmail.com>
2024-04-15 21:53:03 +0200

    Updated process of downloading data

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import warnings
 
 from pandas import read_csv
 
-from download_data import getAllTeamsGameLog
+from download_data import getGameLogs, getStatistics
+from get_data import getTeamGameIds
 from utils import generateSeasons
 
 
@@ -10,14 +11,18 @@ if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     teams = read_csv("resources/teams_details.tsv", sep="\t")
     team_ids = teams["id"]
+    seasons = generateSeasons(2018, 2022)
 
-
-    for season in generateSeasons(2018, 2023):
-        logs = getAllTeamsGameLog(team_ids, season)
-        logs.to_csv(f"resources/game-logs/season_{season}.tsv", sep="\t", index=False)
+    for season in seasons:
+        logs = getGameLogs(team_ids, season)
+        game_ids = getTeamGameIds(logs)
+        perGameStatistics = getStatistics(game_ids)
+        logs.to_csv(f"resources/game-logs-test/season_{season}.tsv", sep="\t", index=False)
+        perGameStatistics.to_csv(f"resources/per-game-statistics/season_{season}.tsv", sep="\t", index=False)
 
 
 # TODO:
+#   0. Download new data
 #   1. ELO rating
 #   2. Check relevance for all parameters (ANOVA or logistic regression)
 #   3. Feature selection
