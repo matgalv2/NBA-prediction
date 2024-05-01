e073df12a2c2f63de9e7ae2d623ce69e97e23879
Mateusz <matgalv2@gmail.com>
2024-05-01 11:51:35 +0200

    Updated

--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
-def generateSeasons(start, end):
-    for year in range(start, end + 1):
+def generateSeasons(first, last):
+    for year in range(first, last + 1):
         yield f"{year}-{str(year + 1)[2:]}"
