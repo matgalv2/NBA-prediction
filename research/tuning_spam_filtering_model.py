0c6b32198082cbc104c8eb5029da281b43f14f4f
Mateusz <matgalv2@gmail.com>
2024-05-22 21:28:23 +0200

    Added scripts for tuning spam filtering model parameters

---
+++ research/tuning_spam_filtering_model.py
@@ -0,0 +1,66 @@
+from typing import TextIO
+
+import pandas as pd
+import os
+
+from sklearn import svm
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import KFold, GridSearchCV
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import *
+
+def hypertune_svm(x, y, file: TextIO, technique:str):
+    model = svm.SVC()
+    kernel = ['linear', 'poly', 'rbf', 'sigmoid']
+    C = [1000, 100, 50, 10, 1.0, 0.1, 0.01]
+    gamma = ['scale']
+    degree = range(1,11)
+
+    grid = dict(kernel=kernel,C=C,gamma=gamma, degree=degree)
+
+    cv = KFold(n_splits=10)
+    grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='precision', error_score=0)
+    grid_result = grid_search.fit(x, y)
+
+    means = grid_result.cv_results_['mean_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, param in zip(means, params):
+        c,degree, gamma, kernel = param["C"], param["degree"], param["gamma"], param["kernel"]
+        if kernel != "poly":
+            degree = "-"
+        file.write(f"{technique}\t{kernel}\t{c}\t{gamma}\t{degree}\t{round(mean,5)}\n")
+
+if __name__ == '__main__':
+    path = "X:/ProgramFiles/JetBrains/PycharmProjects/NBA-predictor/resources"
+
+    filenames = os.listdir(path + '/spam-filtering')
+    videos = [pd.read_csv(path + '/spam-filtering/' + filename, sep=',') for filename in filenames]
+
+    dataset = pd.concat(videos)
+
+    nltk.download('wordnet')
+
+    stop_words = stopwords.words("english")
+    vectorizer = CountVectorizer(stop_words=stop_words)
+
+    Y = dataset["CLASS"].tolist()
+
+    wnl = WordNetLemmatizer()
+    stemmer = PorterStemmer()
+
+
+    with open(path + "/researches/spam-filtering.tsv", 'a') as file:
+        file.write(f"technique\tkernel\tC\tgamma\tdegree\tprecision\n")
+
+        X_lem = vectorizer.fit_transform(dataset["CONTENT"].apply(wnl.lemmatize))
+        hypertune_svm(X_lem, Y, file, "lemmatization")
+
+        X_stem = vectorizer.fit_transform(dataset["CONTENT"].apply(stemmer.stem))
+        hypertune_svm(X_stem, Y, file, "stemming")
+
+
+
+
+
