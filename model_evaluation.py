be25e6791dee918535a3c27fc58fd606ee4f33ca
Mateusz <matgalv2@gmail.com>
2024-06-05 01:47:32 +0200

    Updated

--- model_evaluation.py
+++ model_evaluation.py
@@ -1,5 +1,14 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import sklearn
+from matplotlib import pyplot as plt
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.metrics import *
 
 import utils
 
@@ -19,17 +28,17 @@ columns_to_be_dropped = ['GAME_ID', 'MATCHUP','TEAM_ID_home','TEAM_ABBREVIATION_
 raw_dataset = pd.read_csv('resources/datasets/data.csv', sep='\t')
 
 
+train_raw_dataset = raw_dataset.sample(frac=0.9, random_state=0)
+test_raw_dataset = raw_dataset.drop(train_raw_dataset.index)
 
-test_raw_dataset = utils.calculateAverageStatisticsForLast3Games(raw_dataset.sample(frac=0.1, random_state=0))
+test_raw_dataset_rebased = utils.calculateAverageStatisticsForLast3Games(test_raw_dataset)
 
-raw_dataset.loc[test_raw_dataset.index] = test_raw_dataset
+raw_dataset.loc[test_raw_dataset_rebased.index] = test_raw_dataset_rebased
 
-
-# raw_dataset = utils.normalizeHomeAndAwayTeamsTogether(raw_dataset)
 
 dataset = utils.normalizeHomeAndAwayTeamsTogether(raw_dataset).drop(columns=columns_to_be_dropped)
 
-test_dataset = dataset.loc[test_raw_dataset.index]
+test_dataset = dataset.loc[test_raw_dataset_rebased.index]
 train_dataset = dataset.drop(test_dataset.index)
 
 train_features = train_dataset.copy()
@@ -40,8 +49,115 @@ test_labels = test_features.pop('OUTCOME')
 
 
 
+results = {
+    "basic": [],
+    "norm": [],
+    "no_sent": [],
+    "no_elo": []
+}
 
-print(dataset.describe().transpose())
+# random_state random forest - 1
 
 
 
+
+from sklearn import metrics, svm
+
+
+
+
+model = svm.SVC(probability=True, kernel='poly', C=1.0, degree=3)
+
+model2 = LogisticRegression(solver='liblinear', C=1, penalty='l1')
+model3 = GaussianNB()
+
+
+
+
+model.fit(train_features, train_labels)
+model2.fit(train_features, train_labels)
+model3.fit(train_features, train_labels)
+
+
+#set up plotting area
+plt.figure(0).clf()
+
+#fit logistic regression model and plot ROC curve
+
+y_pred = model.predict_proba(test_features)[:, 1]
+fpr, tpr, _ = metrics.roc_curve(test_labels, y_pred)
+auc = round(metrics.roc_auc_score(test_labels, y_pred), 4)
+plt.plot(fpr,tpr,label=f"SVM (AUC={auc})")
+
+
+y_pred = model2.predict_proba(test_features)[:, 1]
+fpr, tpr, _ = metrics.roc_curve(test_labels, y_pred)
+auc = round(metrics.roc_auc_score(test_labels, y_pred), 4)
+plt.plot(fpr,tpr,label=f"Logistic regression (AUC={auc}")
+
+y_pred = model2.predict_proba(test_features)[:, 1]
+fpr, tpr, _ = metrics.roc_curve(test_labels, y_pred)
+auc = round(metrics.roc_auc_score(test_labels, y_pred), 4)
+plt.plot(fpr,tpr,label=f"Logistic regression (AUC={auc}")
+
+#add legend
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for random_state in range(10):
+#     print("Random state {}".format(random_state), "--------------------------------------")
+#     model = MLPClassifier(hidden_layer_sizes=(128,), activation='relu',alpha=0.01, solver='sgd', learning_rate='adaptive', learning_rate_init=0.0001, max_iter=5000, random_state=random_state)
+#     # model = RandomForestClassifier(n_estimators=20, max_features=14, max_depth=60, min_samples_split=4, min_samples_leaf=4, bootstrap=True, random_state=random_state)
+#     # model = LogisticRegression(solver='liblinear', C=1, penalty='l1', random_state=random_state)
+#     # model = GaussianNB()
+#     # model = svm.SVC(kernel='poly', C=1.0, degree=3)
+#     model.fit(train_features, train_labels)
+#     results["norm"].append(accuracy_score(test_labels, model.predict(test_features)))
+#
+#     model = MLPClassifier(hidden_layer_sizes=(128,), activation='relu',alpha=0.01, solver='sgd', learning_rate='adaptive', learning_rate_init=0.0001, max_iter=5000, random_state=random_state)
+#     # model = RandomForestClassifier(n_estimators=20, max_features=14, max_depth=60, min_samples_split=4, min_samples_leaf=4, bootstrap=True, random_state=random_state)
+#     # model = LogisticRegression(solver='liblinear', C=1, penalty='l1', random_state=random_state)
+#     # model = GaussianNB()
+#     # model = svm.SVC(kernel='poly', C=1.0, degree=3)
+#     model.fit(train_features.drop(columns=["ELO_home", "ELO_away"]), train_labels)
+#     results["no_elo"].append(accuracy_score(test_labels, model.predict(test_features.drop(columns=["ELO_home", "ELO_away"]))))
+#
+#     model = MLPClassifier(hidden_layer_sizes=(128,), activation='relu',alpha=0.01, solver='sgd', learning_rate='adaptive', learning_rate_init=0.0001, max_iter=5000, random_state=random_state)
+#     # model = RandomForestClassifier(n_estimators=20, max_features=14, max_depth=60, min_samples_split=4, min_samples_leaf=4, bootstrap=True, random_state=random_state)
+#     # model = LogisticRegression(solver='liblinear', C=1, penalty='l1', random_state=random_state)
+#     # model = GaussianNB()
+#     # model = svm.SVC(kernel='poly', C=1.0, degree=3)
+#     model.fit(train_features.drop(columns=["SENTIMENT_home", "SENTIMENT_away"]), train_labels)
+#     results["no_sent"].append(accuracy_score(test_labels, model.predict(test_features.drop(columns=["SENTIMENT_home", "SENTIMENT_away"]))))
+#
+#     model = MLPClassifier(hidden_layer_sizes=(128,), activation='relu',alpha=0.01, solver='sgd', learning_rate='adaptive', learning_rate_init=0.0001, max_iter=5000, random_state=random_state)
+#     # model = RandomForestClassifier(n_estimators=20, max_features=14, max_depth=60, min_samples_split=4, min_samples_leaf=4, bootstrap=True, random_state=random_state)
+#     # model = LogisticRegression(solver='liblinear', C=1, penalty='l1', random_state=random_state)
+#     # model = GaussianNB()
+#     # model = svm.SVC(kernel='poly', C=1.0, degree=3)
+#     model.fit(train_features.drop(columns=["SENTIMENT_home", "SENTIMENT_away", "ELO_home", "ELO_away"]), train_labels)
+#     results["basic"].append(
+#         accuracy_score(test_labels, model.predict(test_features.drop(columns=["SENTIMENT_home", "SENTIMENT_away", "ELO_home", "ELO_away"]))))
+#
+#     for key, value in results.items():
+#         print(key, np.mean(value))
